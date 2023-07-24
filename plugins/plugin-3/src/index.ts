0ec730ea8045f0d841b7f2cb011dec817eb9f0b8
Tomas Coufal <tcoufal@redhat.com>
2023-07-24 17:51:08 +0200

    feat(plugins): update them all again 2
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>

--- plugins/plugin-3/src/index.ts
+++ plugins/plugin-3/src/index.ts
@@ -3,3 +3,4 @@ export type Plugin2Type = string;
 export const PLUGIN_NAME = 'plugin-2';
 export const UPDATE = 'foo';
 export const UPDATE2 = 'foo';
+export const UPDATE3 = 'foo';
--- plugins/plugin-5/src/index.ts
+++ plugins/plugin-5/src/index.ts
@@ -3,3 +3,4 @@ export type Plugin1Type = string;
 export const PLUGIN_NAME = 'plugin-1';
 export const UPDATE = 'foo';
 export const UPDATE2 = 'foo';
+export const UPDATE3 = 'foo';
