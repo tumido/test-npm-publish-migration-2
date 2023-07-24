fc787aa160288a524e2bb06d5c1ab3c72f8e0774
Tomas Coufal <tcoufal@redhat.com>
2023-07-24 17:21:17 +0200

    feat(plugins): update them all
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>

--- plugins/plugin-2/src/index.ts
+++ plugins/plugin-2/src/index.ts
@@ -1,3 +1,4 @@
 export type Plugin2Type = string;
 
 export const PLUGIN_NAME = 'plugin-2';
+export const UPDATE = 'foo';
--- plugins/plugin-22/src/index.ts
+++ plugins/plugin-22/src/index.ts
@@ -1,3 +1,4 @@
 export type Plugin1Type = string;
 
 export const PLUGIN_NAME = 'plugin-1';
+export const UPDATE = 'foo';
